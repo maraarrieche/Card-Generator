7b46296cf2d33944383d745c00812e8e67acbcc3
maraarrieche <arrieche.mara@gmail.com>
2023-03-28 22:03:31 +0000

    random card primer intento

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(randomCard) {
+  const pintas = ["♦", "♥", "♠", "♣"];
+  const valores = ["A", "J", "Q", "K"];
+
+  const simbolo = randomCard(pintas);
+  const valor = randomCard(valores);
+
+  let simboloArriba = document.getElementById("simboloArriba");
+  simboloArriba.textContent = simbolo;
+
+  let simboloAbajo = document.getElementById("simboloAbajo");
+  simboloAbajo.textContent = simbolo;
+
+  let valorCentro = document.getElementById("valorCentro");
+  valorCentro.textContent = valor;
+
+  function randomCard(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 };
